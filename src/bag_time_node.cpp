8cabba2dd363236f9530d433c748a7486e55460d
ethankwok <guoyangquan@gs-robot.com>
2019-09-24 17:26:00 +0800

    add cameraInfo into bag

--- src/bag_time_node.cpp
+++ src/bag_time_node.cpp
@@ -30,39 +30,43 @@ int main(int argc, char **argv)
 
     ros::init(argc, argv, "bag_time_node");
     ros::NodeHandle nh_;
-    rosbag::Bag bag1;//d435 depth
+    rosbag::Bag bag1;//t265
     rosbag::Bag bag_result;
 
-    bag1.open("/media/ethan/LENOVO_USB_HDD/Dataset_drf/713_3/3d/record-d400-filtered_depth.bag", rosbag::bagmode::Read);
-    cout<<"d400_depth.bag opened"<<endl;
-    bag_result.open("/media/ethan/LENOVO_USB_HDD/Dataset_drf/713_3/3d/record-d400_depth_processed.bag", rosbag::bagmode::Write);
+    bag1.open("/home/ethan/Documents/lifelong/bag_factory/vins-t265-1-mono-format.bag", rosbag::bagmode::Read);
+    cout<<"vins-t265-1-mono-format.bag opened"<<endl;
+    bag_result.open("/home/ethan/Documents/lifelong/bag_factory/vins-t265-1-mono-format-cameraInfo.bag", rosbag::bagmode::Write);
 
-    //d435
+    //t265
     std::vector<std::string> topics;
-    topics.push_back(std::string("/device_0/sensor_0/Depth_0/image/data"));int d435depthcnt=0;
+    topics.push_back(std::string("/device_0/sensor_0/Fisheye_1/info/camera_info"));int d435depthcnt=0;
+    topics.push_back(std::string("/device_0/sensor_0/Fisheye_1/image/data"));
     cout<<"reading and writing...."<<endl;
-
+    sensor_msgs::CameraInfo cameraInfo;
     rosbag::View view(bag1, rosbag::TopicQuery(topics));
     foreach(rosbag::MessageInstance const m, view)
     {
+        sensor_msgs::CameraInfoConstPtr scameraInfo = m.instantiate<sensor_msgs::CameraInfo>();
+        if(scameraInfo != NULL)
+        {
+            cameraInfo = *scameraInfo;
+        }
+
         sensor_msgs::Image::ConstPtr sdimage = m.instantiate<sensor_msgs::Image>();
         ros::Time bag_time=m.getTime();
         if (sdimage != NULL) {
             d435depthcnt++;
-            sensor_msgs::Image depthimg = *sdimage;
-            for(int i=sdimage->height*5/6*sdimage->step;i<sdimage->height*sdimage->step;i++)
-            {
-                depthimg.data[i]=0;
-            }
-            bag_result.write("/device_0/sensor_0/Depth_0/image/data",bag_time,depthimg);
+            cameraInfo.header.stamp=sdimage->header.stamp;
+            bag_result.write("/device_0/sensor_0/Fisheye_1/info/camera_info",bag_time,cameraInfo);
+            bag_result.write("/device_0/sensor_0/Fisheye_2/info/camera_info",bag_time,cameraInfo);
 
         }
     }
 
     bag1.close();
     bag_result.close();
-    cout<<"d435depthcnt "<<d435depthcnt<<endl;
-    cout<<"==========depth writing done!============="<<endl;
+    cout<<"cameraInfo cnt "<<d435depthcnt<<endl;
+    cout<<"==========cameraInfo writing done!============="<<endl;
 
     return 0;
 }
