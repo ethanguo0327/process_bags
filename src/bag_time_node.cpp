ca3de503d9487c608e663303141adbfa6f50c31b
ethankwok <guoyangquan@gs-robot.com>
2019-09-24 20:14:17 +0800

    write timestamp to csv file

--- src/bag_time_node.cpp
+++ src/bag_time_node.cpp
@@ -31,42 +31,29 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "bag_time_node");
     ros::NodeHandle nh_;
     rosbag::Bag bag1;//t265
-    rosbag::Bag bag_result;
-
+    std::ofstream f2;
+    f2.open("/home/ethan/Documents/lifelong/bag_factory/vins-t265-1-timestamp.csv",std::ios_base::out|std::ios_base::trunc);
     bag1.open("/home/ethan/Documents/lifelong/bag_factory/vins-t265-1-mono-format.bag", rosbag::bagmode::Read);
     cout<<"vins-t265-1-mono-format.bag opened"<<endl;
-    bag_result.open("/home/ethan/Documents/lifelong/bag_factory/vins-t265-1-mono-format-cameraInfo.bag", rosbag::bagmode::Write);
 
     //t265
     std::vector<std::string> topics;
-    topics.push_back(std::string("/device_0/sensor_0/Fisheye_1/info/camera_info"));int d435depthcnt=0;
-    topics.push_back(std::string("/device_0/sensor_0/Fisheye_1/image/data"));
+    topics.push_back(std::string("/device_0/sensor_0/Fisheye_1/image/data"));int d435depthcnt=0;
     cout<<"reading and writing...."<<endl;
-    sensor_msgs::CameraInfo cameraInfo;
     rosbag::View view(bag1, rosbag::TopicQuery(topics));
     foreach(rosbag::MessageInstance const m, view)
     {
-        sensor_msgs::CameraInfoConstPtr scameraInfo = m.instantiate<sensor_msgs::CameraInfo>();
-        if(scameraInfo != NULL)
-        {
-            cameraInfo = *scameraInfo;
-        }
-
         sensor_msgs::Image::ConstPtr sdimage = m.instantiate<sensor_msgs::Image>();
         ros::Time bag_time=m.getTime();
         if (sdimage != NULL) {
             d435depthcnt++;
-            cameraInfo.header.stamp=sdimage->header.stamp;
-            bag_result.write("/device_0/sensor_0/Fisheye_1/info/camera_info",bag_time,cameraInfo);
-            bag_result.write("/device_0/sensor_0/Fisheye_2/info/camera_info",bag_time,cameraInfo);
-
+            f2<<sdimage->header.stamp<<std::endl;
         }
     }
 
     bag1.close();
-    bag_result.close();
-    cout<<"cameraInfo cnt "<<d435depthcnt<<endl;
-    cout<<"==========cameraInfo writing done!============="<<endl;
+    cout<<"timestamp cnt "<<d435depthcnt<<endl;
+    cout<<"==========timestamp writing done!============="<<endl;
 
     return 0;
 }
